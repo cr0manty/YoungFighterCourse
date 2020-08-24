ed3348aabef1ad9014bbc6d90a2d2bd1b8afc500
cr0manty <aappland@gmail.com>
2020-08-24 16:47:14 +0300

    move files, delete unused

--- YoungFighterCourse/Models/RequestManager.m
+++
@@ -1,9 +0,0 @@
-//
-//  RequestManager.m
-//  YoungFighterCourse
-//
-//  Created by Денис on 14.08.2020.
-//  Copyright © 2020 cr0manty. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
